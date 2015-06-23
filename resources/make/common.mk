2004695e95a821b4b29bc26b5dfcf68c29dfd078
arpunk <arpunk@cryptolab.net>
2015-06-22 21:13:22 -0500

    Cleanup some unused rebar.cmd calls

--- resources/make/common.mk
+++ resources/make/common.mk
@@ -53,15 +53,10 @@ clean-eunit:
 
 compile: get-deps clean-ebin
 	@echo "Compiling project code and dependencies ..."
-	@which rebar.cmd >/dev/null 2>&1 && \
-	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) rebar.cmd compile || \
 	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) rebar compile
 
 compile-no-deps: clean-ebin
 	@echo "Compiling only project code ..."
-	@which rebar.cmd >/dev/null 2>&1 && \
-	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) \
-	rebar.cmd compile skip_deps=true || \
 	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) rebar compile skip_deps=true
 
 compile-tests: clean-eunit
